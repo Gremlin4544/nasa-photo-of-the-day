5b68b760237a469f0ee230660c6b2d81f910de26
Gremlin4544 <alve@mckethen.com>
2020-01-24 11:07:40 -0500

    added style to card component

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.p`
+            font-size: 1.25em;
+            padding: 0 15%;
+            color: darkgrey;
+            font-weight: bold;
+            `;
 
 const CardComponent = props => {
     return(
         <div>
             <div className="nasa_card" key={props.id}>
                 <img className="nasa_img" alt="Nasa POTD" src={props.url} />
-                <h3>Title: {props.title}</h3>
+                <Title>Title: {props.title}</Title>
                 <p>Date: {props.date}</p>
                 <div>
                     <p>{props.explanation}</p>
